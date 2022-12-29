15dfd6176343565fc5843e58ad287b18fd0fa5e4
Karthik Raja <92788387+karthikraja-arumugam@users.noreply.github.com>
2022-12-29 17:14:19 +0530

    Getting started sample added

---
+++ GettingStarted/DataFormViewModel/DataFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GettingStarted
+{
+    public class DataFormViewModel
+    {
+        public ContactsInfo ContactsInfo { get; set; }
+
+        public DataFormViewModel()
+        {
+            this.ContactsInfo = new ContactsInfo();
+        }
+    }
+}
