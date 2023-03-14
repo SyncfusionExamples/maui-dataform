670ea88e4ae825668bea63666d02967571d6949d
Kerubananthan-Ganesan <104969840+Kerubananthan3761@users.noreply.github.com>
2023-03-14 13:05:08 +0530

    Added Floating label layout sample

---
+++ FloatingLabelLayoutSample/FloatingLabelLayoutSample/Behavior/FloatingLabelBehavior.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Maui.DataForm;
+
+namespace FloatingLabelLayoutSample
+{
+    internal class FloatingLabelBehavior : Behavior<SfDataForm>
+    {
+        protected override void OnAttachedTo(SfDataForm dataForm)
+        {
+            base.OnAttachedTo(dataForm);
+            dataForm.GenerateDataFormItem += OnGenerateDataFormItem;
+        }
+
+        private void OnGenerateDataFormItem(object sender, GenerateDataFormItemEventArgs e)
+        {
+            if (e.DataFormItem != null)
+            {
+                if (e.DataFormItem.FieldName == nameof(FloatingLabelModel.FirstName))
+                {
+                    e.DataFormItem.ValidMessageLabelTextStyle = new DataFormTextStyle { TextColor = Colors.GreenYellow, FontAttributes = FontAttributes.Italic };
+                    e.DataFormItem.LeadingView = new Label { Text = "F", FontSize = 18, TextColor = Colors.Gray, FontFamily = "InputLayoutIcons", HeightRequest = 24, VerticalTextAlignment = TextAlignment.End };
+                    e.DataFormItem.TextInputLayoutSettings = new TextInputLayoutSettings
+                    {
+                        ContainerType = TextInputLayoutContainerType.Outlined,
+                        HelperTextStyle = new DataFormTextStyle { TextColor = Colors.Violet, FontAttributes = FontAttributes.Bold },
+                    };
+                }
+                else if (e.DataFormItem.FieldName == nameof(FloatingLabelModel.MiddleName))
+                {
+                    e.DataFormItem.ErrorLabelTextStyle = new DataFormTextStyle { TextColor = Colors.Indigo, FontAttributes = FontAttributes.Italic };
+                    e.DataFormItem.TextInputLayoutSettings = new TextInputLayoutSettings
+                    {
+                        ContainerType = TextInputLayoutContainerType.Filled,
+                        OutlineCornerRadius = 10,
+                        Stroke = Colors.BlueViolet,
+                    };
+                }
+                else if (e.DataFormItem.FieldName == nameof(FloatingLabelModel.LastName))
+                {
+                    e.DataFormItem.TextInputLayoutSettings = new TextInputLayoutSettings
+                    {
+                        ContainerType = TextInputLayoutContainerType.None,
+                        FocusedStroke = Colors.AliceBlue,
+                    };
+                }
+                else if (e.DataFormItem.FieldName == nameof(FloatingLabelModel.ContactNumber))
+                {
+                    e.DataFormItem.TrailingView = new Label { Text = "E", FontSize = 18, TextColor = Colors.Gray, FontFamily = "InputLayoutIcons", HeightRequest = 24, VerticalTextAlignment = TextAlignment.End };
+                    e.DataFormItem.TrailingViewPosition = TextInputLayoutViewPosition.Inside;
+                }
+                else if (e.DataFormItem.FieldName == nameof(FloatingLabelModel.Email))
+                {
+                    e.DataFormItem.TextInputLayoutSettings = new TextInputLayoutSettings
+                    {
+                        FocusedStrokeThickness = 3.0,
+                        UnfocusedStrokeThickness = 4.0,
+                    };
+                }
+                else if (e.DataFormItem.FieldName == nameof(FloatingLabelModel.Address))
+                {
+                    e.DataFormItem.LabelTextStyle = new DataFormTextStyle { FontAttributes = FontAttributes.Italic, TextColor = Colors.BlueViolet };
+                }
+            }
+
+        }
+
+        protected override void OnDetachingFrom(SfDataForm dataForm)
+        {
+            base.OnDetachingFrom(dataForm);
+        }
+    }
+}
---
+++ FloatingLabelLayoutSample/FloatingLabelLayoutSample/Model/FloatingLabelModel.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.Maui.DataForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FloatingLabelLayoutSample
+{
+    internal class FloatingLabelModel
+    {
+        [Display(Name = "First Name", Prompt = "Enter your first name")]
+        [DataFormDisplayOptions(ValidMessage = "Valid name")]
+        [Required(ErrorMessage = "Please enter your first name")]
+        [StringLength(10, ErrorMessage = "First name should not exceed 10 characters")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Middle Name")]
+        [Required(ErrorMessage = "Please enter your middle name")]
+        [StringLength(10, ErrorMessage = "First name should not exceed 10 characters")]
+        public string MiddleName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please enter your last name")]
+        [StringLength(10, ErrorMessage = "First name should not exceed 10 characters")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Contact Number")]
+        public string ContactNumber { get; set; }
+
+        [Display(Name = "Email", Prompt = "Enter your email")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}
